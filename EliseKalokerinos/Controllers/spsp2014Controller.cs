00d8e0c705916b218765fa9885451eafdad06ea1
Ben Cull <bjcull@gmail.com>
2014-02-11 23:16:48 +1000

    Added spsp2014 page

---
+++ EliseKalokerinos/Controllers/spsp2014Controller.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EliseKalokerinos.Controllers
+{
+    public class spsp2014Controller : Controller
+    {
+        //
+        // GET: /spsp2014/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
