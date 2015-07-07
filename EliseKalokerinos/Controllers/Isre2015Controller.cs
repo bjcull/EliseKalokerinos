d9b7decf93fa31acd1d1625557c03e6043b90d4f
Ben Cull <bjcull@gmail.com>
2015-07-07 11:44:10 +0200

    Updated CV and added ISRE document

---
+++ EliseKalokerinos/Controllers/Isre2015Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EliseKalokerinos.Controllers
+{
+    public class Isre2015Controller : Controller
+    {
+        // GET: Isre2015
+        public ActionResult Index()
+        {
+            return new FilePathResult("~/Documents/ISRE 2015 poster Elise Kalokerinos.pdf", "application/pdf");
+        }
+    }
+}
